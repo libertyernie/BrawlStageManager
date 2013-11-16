47bd1f7011700789eaabb822982f8d6ac1cddb7a
libertyernie <libertyernie0@gmail.com>
2013-11-16 17:17:43 -0600

    Update readme and hide a stray label on startup

--- StageManager/PortraitViewer.Designer.cs
+++ StageManager/PortraitViewer.Designer.cs
@@ -27,18 +27,18 @@
 			this.label1 = new System.Windows.Forms.Label();
 			this.saveButton = new System.Windows.Forms.Button();
 			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+			this.prevbase = new BrawlStageManager.ImagePreviewPanel();
+			this.icon = new BrawlStageManager.ImagePreviewPanel();
 			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
 			this.btnGenerateName = new System.Windows.Forms.Button();
+			this.lblIconTex = new System.Windows.Forms.Label();
+			this.frontstname = new BrawlStageManager.ImagePreviewPanel();
+			this.seriesicon = new BrawlStageManager.ImagePreviewPanel();
+			this.selmap_mark = new BrawlStageManager.ImagePreviewPanel();
 			this.fileSizeBar = new System.Windows.Forms.ProgressBar();
 			this.fileSizeLabel = new System.Windows.Forms.Label();
 			this.modifyPAT0 = new System.Windows.Forms.Button();
 			this.button1 = new System.Windows.Forms.Button();
-			this.lblIconTex = new System.Windows.Forms.Label();
-			this.prevbase = new BrawlStageManager.ImagePreviewPanel();
-			this.icon = new BrawlStageManager.ImagePreviewPanel();
-			this.frontstname = new BrawlStageManager.ImagePreviewPanel();
-			this.seriesicon = new BrawlStageManager.ImagePreviewPanel();
-			this.selmap_mark = new BrawlStageManager.ImagePreviewPanel();
 			this.flowLayoutPanel1.SuspendLayout();
 			this.tableLayoutPanel1.SuspendLayout();
 			this.SuspendLayout();
@@ -81,6 +81,26 @@
 			this.flowLayoutPanel1.Size = new System.Drawing.Size(210, 500);
 			this.flowLayoutPanel1.TabIndex = 3;
 			// 
+			// prevbase
+			// 
+			this.prevbase.AllowDrop = true;
+			this.prevbase.BorderChangeItemEnabled = false;
+			this.prevbase.Location = new System.Drawing.Point(0, 0);
+			this.prevbase.Margin = new System.Windows.Forms.Padding(0);
+			this.prevbase.Name = "prevbase";
+			this.prevbase.Size = new System.Drawing.Size(176, 176);
+			this.prevbase.TabIndex = 0;
+			// 
+			// icon
+			// 
+			this.icon.AllowDrop = true;
+			this.icon.BorderChangeItemEnabled = true;
+			this.icon.Location = new System.Drawing.Point(0, 176);
+			this.icon.Margin = new System.Windows.Forms.Padding(0);
+			this.icon.Name = "icon";
+			this.icon.Size = new System.Drawing.Size(64, 56);
+			this.icon.TabIndex = 3;
+			// 
 			// tableLayoutPanel1
 			// 
 			this.tableLayoutPanel1.ColumnCount = 1;
@@ -107,6 +127,44 @@
 			this.btnGenerateName.UseVisualStyleBackColor = true;
 			this.btnGenerateName.Visible = false;
 			this.btnGenerateName.Click += new System.EventHandler(this.btnGenerateName_Click);
+			// 
+			// lblIconTex
+			// 
+			this.lblIconTex.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.lblIconTex.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblIconTex.Location = new System.Drawing.Point(3, 0);
+			this.lblIconTex.Name = "lblIconTex";
+			this.lblIconTex.Size = new System.Drawing.Size(114, 28);
+			this.lblIconTex.TabIndex = 11;
+			this.lblIconTex.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// frontstname
+			// 
+			this.frontstname.AllowDrop = true;
+			this.frontstname.BorderChangeItemEnabled = false;
+			this.frontstname.Location = new System.Drawing.Point(0, 238);
+			this.frontstname.Margin = new System.Windows.Forms.Padding(0);
+			this.frontstname.Name = "frontstname";
+			this.frontstname.Size = new System.Drawing.Size(208, 56);
+			this.frontstname.TabIndex = 4;
+			// 
+			// seriesicon
+			// 
+			this.seriesicon.AllowDrop = true;
+			this.seriesicon.BorderChangeItemEnabled = false;
+			this.seriesicon.Location = new System.Drawing.Point(3, 297);
+			this.seriesicon.Name = "seriesicon";
+			this.seriesicon.Size = new System.Drawing.Size(64, 64);
+			this.seriesicon.TabIndex = 7;
+			// 
+			// selmap_mark
+			// 
+			this.selmap_mark.AllowDrop = true;
+			this.selmap_mark.BorderChangeItemEnabled = false;
+			this.selmap_mark.Location = new System.Drawing.Point(73, 297);
+			this.selmap_mark.Name = "selmap_mark";
+			this.selmap_mark.Size = new System.Drawing.Size(120, 56);
+			this.selmap_mark.TabIndex = 8;
 			// 
 			// fileSizeBar
 			// 
@@ -146,65 +204,6 @@
 			this.button1.Text = ">";
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
-			// 
-			// lblIconTex
-			// 
-			this.lblIconTex.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.lblIconTex.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.lblIconTex.Location = new System.Drawing.Point(3, 0);
-			this.lblIconTex.Name = "lblIconTex";
-			this.lblIconTex.Size = new System.Drawing.Size(114, 28);
-			this.lblIconTex.TabIndex = 11;
-			this.lblIconTex.Text = "label2";
-			this.lblIconTex.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-			// 
-			// prevbase
-			// 
-			this.prevbase.AllowDrop = true;
-			this.prevbase.BorderChangeItemEnabled = false;
-			this.prevbase.Location = new System.Drawing.Point(0, 0);
-			this.prevbase.Margin = new System.Windows.Forms.Padding(0);
-			this.prevbase.Name = "prevbase";
-			this.prevbase.Size = new System.Drawing.Size(176, 176);
-			this.prevbase.TabIndex = 0;
-			// 
-			// icon
-			// 
-			this.icon.AllowDrop = true;
-			this.icon.BorderChangeItemEnabled = true;
-			this.icon.Location = new System.Drawing.Point(0, 176);
-			this.icon.Margin = new System.Windows.Forms.Padding(0);
-			this.icon.Name = "icon";
-			this.icon.Size = new System.Drawing.Size(64, 56);
-			this.icon.TabIndex = 3;
-			// 
-			// frontstname
-			// 
-			this.frontstname.AllowDrop = true;
-			this.frontstname.BorderChangeItemEnabled = false;
-			this.frontstname.Location = new System.Drawing.Point(0, 238);
-			this.frontstname.Margin = new System.Windows.Forms.Padding(0);
-			this.frontstname.Name = "frontstname";
-			this.frontstname.Size = new System.Drawing.Size(208, 56);
-			this.frontstname.TabIndex = 4;
-			// 
-			// seriesicon
-			// 
-			this.seriesicon.AllowDrop = true;
-			this.seriesicon.BorderChangeItemEnabled = false;
-			this.seriesicon.Location = new System.Drawing.Point(3, 297);
-			this.seriesicon.Name = "seriesicon";
-			this.seriesicon.Size = new System.Drawing.Size(64, 64);
-			this.seriesicon.TabIndex = 7;
-			// 
-			// selmap_mark
-			// 
-			this.selmap_mark.AllowDrop = true;
-			this.selmap_mark.BorderChangeItemEnabled = false;
-			this.selmap_mark.Location = new System.Drawing.Point(73, 297);
-			this.selmap_mark.Name = "selmap_mark";
-			this.selmap_mark.Size = new System.Drawing.Size(120, 56);
-			this.selmap_mark.TabIndex = 8;
 			// 
 			// PortraitViewer
 			// 
